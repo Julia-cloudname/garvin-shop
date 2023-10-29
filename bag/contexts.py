5a8018c5f7f187a2ea983deab0653728912d4d95
Julia <malukjulia2014@gmail.com>
2023-10-29 13:56:26 +0000

    Added discount logic to the bag

--- bag/contexts.py
+++ bag/contexts.py
@@ -3,20 +3,25 @@ from django.conf import settings
 from django.shortcuts import get_object_or_404
 from products.models import Product
 
-def calculate_price_based_on_quantity(quantity):
+def calculate_price_based_on_quantity(quantity, base_price):
+    discount = Decimal(0)   # Задаем начальное значение discount как Decimal
     if 0 <= quantity <= 9:
-        return 47
+        discount = Decimal(0)
     elif 10 <= quantity <= 59:
-        return 35
+        discount = base_price * Decimal(0.25)   # Преобразуем 0.25 в Decimal
     elif 60 <= quantity <= 179:
-        return 30
+        discount = base_price * Decimal(0.36)   # Преобразуем 0.35 в Decimal
     elif 180 <= quantity <= 299:
-        return 23
+        discount = base_price * Decimal(0.52)   # Преобразуем 0.45 в Decimal
     else:
-        return 20
+        discount = base_price * Decimal(0.57)   # Преобразуем 0.50 в Decimal
+
+    adjusted_price = base_price - discount
+    adjusted_price = Decimal(round(float(adjusted_price)))
+
+    return adjusted_price
 
 def bag_contents(request):
-
     bag_items = []
     grand_total = 0
     product_count = 0
@@ -24,8 +29,8 @@ def bag_contents(request):
 
     for item_id, quantity in bag.items():
         product = get_object_or_404(Product, pk=item_id)
-        
-        adjusted_price = calculate_price_based_on_quantity(quantity)
+    
+        adjusted_price = calculate_price_based_on_quantity(quantity, product.price)
         
         grand_total += quantity * adjusted_price
         product_count += quantity
@@ -35,6 +40,7 @@ def bag_contents(request):
             'quantity': quantity,
             'product': product,
             'adjusted_price': adjusted_price,
+            'base_price': product.price,
         })
     
     context = {

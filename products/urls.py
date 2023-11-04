1777c7f241315cf7e532923d461b7203a987bdfc
Julia <malukjulia2014@gmail.com>
2023-11-04 20:19:37 +0000

    Product Admin - add product view, url and template

--- products/urls.py
+++ products/urls.py
@@ -1,12 +1,8 @@
 from django.urls import path
 from . import views
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.all_products, name='products'),
-    path('<int:product_id>', views.product_detail, name='product_detail')
+    path('<int:product_id>/', views.product_detail, name='product_detail'),
+    path('add/', views.add_product, name='add_product'),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

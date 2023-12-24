1ecd27d3b4d9fdc9d4b8ee78edf81cba292b4d27
Julia Maluk <juliamaluk@julias-mbp-2.home>
2023-12-24 19:46:07 +0000

    Added remove from history functionality

--- history/views.py
+++ history/views.py
@@ -2,14 +2,38 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from .models import ProductViewHistory
 from products.models import Product
+from django.utils import timezone
+from django.contrib import messages
 
 @login_required
 def view_history(request):
+    # Retrieves the viewing history of the currently logged-in user
     user_history = ProductViewHistory.objects.filter(user=request.user)
     return render(request, 'history/history.html', {'history': user_history})
 
 @login_required
 def add_to_history(request, product_id):
     product = get_object_or_404(Product, pk=product_id)
-    ProductViewHistory.objects.create(user=request.user, product=product)
-    return redirect('product_detail', product_id=product_id)
+    # Creates or updates the viewing history entry for the product
+    history_entry, created = ProductViewHistory.objects.get_or_create(
+        user=request.user, 
+        product=product,
+        defaults={'created_at': timezone.now()}
+    )
+    # Updates the viewing timestamp if the history entry already existed
+    if not created:
+        history_entry.created_at = timezone.now()
+        history_entry.save()
+    return redirect('product_detail', product_id=product.id)
+
+@login_required
+def remove_from_history(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    # Deletes the viewing history entry for the product
+    history_entry = ProductViewHistory.objects.get(
+        user=request.user, 
+        product=product
+    )
+    history_entry.delete()
+    messages.success(request, f'{product.name} was removed from your viewing history')
+    return redirect('view_history')

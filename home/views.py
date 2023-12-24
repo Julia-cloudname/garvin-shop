c1f7f9385faaccfadee8c578dbedc40182caea9c
Julia Maluk <juliamaluk@julias-mbp-2.home>
2023-12-24 20:38:35 +0000

    Fixed the bag with main page displayed for new users

--- home/views.py
+++ home/views.py
@@ -2,14 +2,36 @@ from django.shortcuts import render
 from products.models import Product
 from reviews.models import Review
 from history.models import ProductViewHistory
+from django.contrib.auth.decorators import login_required
+
+from django.shortcuts import render
+from products.models import Product
+from reviews.models import Review
+from history.models import ProductViewHistory
 
 def index(request):
     """ A view to return the index page """
 
     latest_reviews = Review.objects.order_by('-created_at')[:4]
     products = Product.objects.all()[:6] 
-    home_view_history = ProductViewHistory.objects.filter(user=request.user)[:6]
-    return render(request, 'home/index.html', {'latest_reviews': latest_reviews, 'products': products, 'home_view_history': home_view_history})
+
+    # Initialize an empty query set for home_view_history
+    home_view_history = None
+
+    # Check if user is authenticated
+    if request.user.is_authenticated:
+        # Fetch the user's view history if they are authenticated
+        home_view_history = ProductViewHistory.objects.filter(user=request.user)[:6]
+
+    # Pass the history to the template, will be None if user is not authenticated
+    return render(request, 'home/index.html', {
+        'latest_reviews': latest_reviews, 
+        'products': products, 
+        'home_view_history': home_view_history
+    })
+
+# Other views...
+
 
 def contact(request):
     """ A view to return the contact page """
